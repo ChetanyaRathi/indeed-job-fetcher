da3e90d66bb7b4a55823c3e7425abb353e4bd9c6
Chetanya Rathi <rathi.chetanya@gmail.com>
2026-07-30 22:35:12 -0400

    Add web service: resume upload, ATS job fetch, ranking + React frontend

--- jobfit/config.py
+++ jobfit/config.py
@@ -9,5 +9,8 @@ EMBED_MODEL = os.getenv("EMBED_MODEL", "nomic-embed-text")
 TOP_K = int(os.getenv("TOP_K", "10"))
 MIN_FIT = int(os.getenv("MIN_FIT", "75"))
 EASY_APPLY_ONLY = os.getenv("EASY_APPLY_ONLY", "true").lower() in ("true", "1", "yes")
+MAX_CONCURRENT_LLM = int(os.getenv("MAX_CONCURRENT_LLM", "1"))
+JOB_REFRESH_HOURS = int(os.getenv("JOB_REFRESH_HOURS", "6"))
+MAX_UPLOAD_MB = int(os.getenv("MAX_UPLOAD_MB", "5"))
 
 DATA_DIR = os.getenv("DATA_DIR", "data")
