4b38869346af0de39aa73431de1b74769bb5f5ee
Chetanya Rathi <rathi.chetanya@gmail.com>
2026-07-30 21:05:48 -0400

    Add Indeed Job Fetcher Phase 1 scaffold

---
+++ jobfit/ingest/scraper.py
@@ -0,0 +1,8 @@
+def scrape_jobs(*args, **kwargs):
+    """
+    Phase 2 stub: ATS / Indeed source scraper.
+    """
+    raise NotImplementedError(
+        "Job scraper is not implemented in Phase 1. "
+        "Please use folder-based ingestion."
+    )
